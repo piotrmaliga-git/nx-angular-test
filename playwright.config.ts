0c722ac964b0512cc0dc7151c942a28479d732db
piotr.maliga <piotr.maliga.work@gmail.com>
2023-11-19 15:09:53 +0100

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
 import { nxE2EPreset } from '@nx/playwright/preset';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:3000';
+const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
 
 /**
  * Read environment variables from file.
@@ -18,15 +19,23 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:3000';
 export default defineConfig({
   ...nxE2EPreset(__filename, { testDir: './e2e' }),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  projects: [
+    {
+      name: 'chrome',
+      use: {
+        channel: 'chrome',
+      },
+    },
+  ],
   use: {
     baseURL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  /* Run your local dev server before starting the tests */ // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  //   cwd: workspaceRoot,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:4200',
+    reuseExistingServer: !process.env.CI,
+    cwd: workspaceRoot,
+  },
 });
